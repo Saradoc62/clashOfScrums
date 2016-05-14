59bd90a251b00084256e47b5dc6ed1f892e26b76
Cyril Cardon <cyrilcardon23@gmail.com>
2016-05-14 15:55:02 +0200

    fix deck unit test

--- model/test/deckTest.cxx
+++ model/test/deckTest.cxx
@@ -1,13 +1,12 @@
 #define BOOST_TEST_MODULE DeckTest
 #include <boost/test/unit_test.hpp>
 
+#include <configuration.h>
 #include <model/deck.hxx>
 
 BOOST_AUTO_TEST_CASE(CheckDeckSize)
 {
-	std::string xmlPath = "res/cards.xml";
-	const int deckSize = 10;
-	
+	std::string xmlPath = std::string(RESOURCES_PATH) + "cards.xml";	const int deckSize = 10;
 	Deck deck(xmlPath, deckSize);
 
 	int i = 0;
